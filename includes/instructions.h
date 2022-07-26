b0f59ae8d57fc36f4fedba693a8024239a42a8e0
alopez-g <alopez-g@student.42madrid.com>
2022-07-27 01:46:37 +0200

    Ult

--- includes/instructions.h
+++ includes/instructions.h
@@ -6,7 +6,7 @@
 /*   By: alopez-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 01:37:51 by alopez-g          #+#    #+#             */
-/*   Updated: 2022/07/26 23:54:44 by alopez-g         ###   ########.fr       */
+/*   Updated: 2022/07/27 00:00:48 by alopez-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@
 # include "push_swap.h"
 
 typedef void	(*t_instr)(t_stack *stack);
-void	sab(t_stack *stacks, t_list **sel, t_list **other);
-void	sa(t_stack *stacks);
-void	sb(t_stack *stacks);
-void	ss(t_stack *stacks);
-void	pab(t_stack *stacks, t_list **sel, t_list **other);
-void	pa(t_stack *stacks);
-void	pb(t_stack *stacks);
-void	rab(t_stack *stacks, t_list **sel, t_list **other);
-void	ra(t_stack *stacks);
-void	rb(t_stack *stacks);
-void	rr(t_stack *stacks);
-void	rrab(t_stack *stacks, t_list **sel, t_list **other);
-void	rra(t_stack *stacks);
-void	rrb(t_stack *stacks);
-void	rrr(t_stack *stacks);
+void	sab(t_stack *st, t_list **sel, t_list **other);
+void	sa(t_stack *st);
+void	sb(t_stack *st);
+void	ss(t_stack *st);
+void	pab(t_stack *st, t_list **sel, t_list **other);
+void	pa(t_stack *st);
+void	pb(t_stack *st);
+void	rab(t_stack *st, t_list **sel, t_list **other);
+void	ra(t_stack *st);
+void	rb(t_stack *st);
+void	rr(t_stack *st);
+void	rrab(t_stack *st, t_list **sel, t_list **other);
+void	rra(t_stack *st);
+void	rrb(t_stack *st);
+void	rrr(t_stack *st);
 void	exec_instr(t_stack *st);
 void	print_instr(t_stack *st);
 void	optimize(t_stack *st);
--- src/instr/optim.c
+++ src/instr/optim.c
@@ -6,7 +6,7 @@
 /*   By: alopez-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 22:49:05 by alopez-g          #+#    #+#             */
-/*   Updated: 2022/07/26 23:55:06 by alopez-g         ###   ########.fr       */
+/*   Updated: 2022/07/27 00:32:50 by alopez-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,42 @@
 #include "ft_printf.h"
 
 /*
+ *
  * Eliminar parejas redundantes de PA y PB
  * Buscar un rango con el mismo numero de PA que de PB
  * Eliminar todas las instrucciones de ese rango
  *
- * Function que chequee si los numeros estan ordenados de antemano
  * */
 void	optimize(t_stack *st)
 {
-	(void)st;
+	int		na;
+	int		nb;
+	t_list	*first;
+	t_list	*last;
+	t_list	*ins;
+
+	(void)first;
+	(void)last;
+	na = 0;
+	nb = 0;
+	ins = st->i;
+	while (ins)
+	{
+		if ((t_instr)ins->content == pa)
+		{
+			na++;
+		}
+		else if ((t_instr)ins->content == pb)
+		{
+			nb++;
+		}
+		if (((t_instr)ins->content != pa && (t_instr)ins->content != pb) || !(ins->next))
+		{
+			if (nb > 0 && na > 0)
+				ft_printf("A(%d) B(%d)\n", na, nb);
+			na = 0;
+			nb = 0;
+		}
+		ins = ins->next;
+	}
 }
